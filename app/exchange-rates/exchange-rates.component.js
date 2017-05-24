ac7850b115129cce644eb96a73ee4d7b515caf24
Taras <slideua@gmail.com>
2017-05-24 15:03:02 +0300

    initial commit

--- app/exchange-rates/exchange-rates.component.js
+++ app/exchange-rates/exchange-rates.component.js
@@ -6,7 +6,7 @@ function ExchangeRatesController() {
 
 // Register `exchangeRates` component, along with its associated controller and template
 angular.module('exchangeRates').component('exchangeRates', {
-    templateUrl: 'app/exchange-rates/exchange-rates.template.html',
+    templateUrl: '/app/exchange-rates/exchange-rates.template.html',
     controller: ExchangeRatesController,
     bindings: {
         exchangeRatesData: '='
